27fa789c1be7e5fd53d7a5e786c8d46c06d8578d
Chanelle Francis <chanelle@viafoura.com>
2022-01-27 11:48:43 -0500

    updating sharebar css

--- components/vf-sharebar.js
+++ components/vf-sharebar.js
@@ -1,6 +1,8 @@
 export default function VfShareBar({ showTotal, orientation }) {
+  const isVertical = orientation === 'vertical';
+  console.log(orientation)
   return (
-    <div className="viafoura">
+    <div className={`viafoura custom-sharebar${isVertical ? '-vertical' : ''}`} style={{minHeight: `${isVertical ? '375px' : '65px'}` }}>
       <div
         className={`vf-widget vf-share-bar-circle vf-share-bar-${orientation}`}
         data-widget="sharebar"
