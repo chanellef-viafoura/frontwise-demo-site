4ea802f68498e4e6f2704eea46fc09458b543528
Chanelle Francis <chanelle@viafoura.com>
2022-01-27 11:58:43 -0500

    updating frontwise url

--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,7 @@ export default class MyDocument extends Document {
           <script
             type="text/javascript"
             async
-            src="//cdn.viafoura.net/front/0.0.19-PR-37-3-5-9c6b52c/index.js"
+            src="//cdn.viafoura.net/front/0.0.19-PR-37-5-7-2674187/index.js"
           ></script>
         </body>
       </Html>
