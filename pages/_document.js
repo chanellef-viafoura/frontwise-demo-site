2e59301ea52f583ac6a3de9584841a7a1e762206
Chanelle Francis <chanelle@viafoura.com>
2022-02-04 17:01:01 -0500

    adding prod end point

--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,7 @@ export default class MyDocument extends Document {
           <script
             type="text/javascript"
             async
-            src="//cdn.viafoura.net/front/0.0.22-PR-41-23-18-aa38fc8/index.js"
+            src="//cdn.viafoura.net/entry/index.js"
           ></script>
         </body>
       </Html>
