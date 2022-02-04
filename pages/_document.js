6abf85ebac84f8408b001b06dcf0b4e28b8b5b17
Chanelle Francis <chanelle@viafoura.com>
2022-02-04 11:02:33 -0500

    adding latest test deploy

--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,7 @@ export default class MyDocument extends Document {
           <script
             type="text/javascript"
             async
-            src="//cdn.viafoura.net/front/0.0.22-PR-41-7-2-0579cbf/index.js"
+            src="//cdn.viafoura.net/front/0.0.22-PR-41-9-4-ca07744/index.js"
           ></script>
         </body>
       </Html>
